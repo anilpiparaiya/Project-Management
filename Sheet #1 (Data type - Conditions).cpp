fc6c93e144a6f8c88bf0b41efe83435e4e5e1102
Anil Piparaiya <78451912+anilpiparaiya@users.noreply.github.com>
2023-02-06 11:49:44 +0530

    Update Sheet #1 (Data type - Conditions).cpp

--- Sheet #1 (Data type - Conditions).cpp
+++ Sheet #1 (Data type - Conditions).cpp
@@ -511,3 +511,46 @@ int main(){
  
  
 }
+
+
+-------------------------------------------------------------------------------------------------------------
+
+// S - Interval 
+ 
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+ 
+ 
+int main(){
+    float n;
+    cin>>n;
+    
+    
+    
+    if(n>=0.0 && n<=25.0){
+        cout<<"Interval ["<<0<<","<<25<<"]";
+    }
+    
+    else if(n> 25.0 && n<=50.0){
+        cout<<"Interval ("<<25<<","<<50<<"]";
+    }
+    else if(n>50.0 && n<=75.0){
+        
+        cout<<"Interval ("<<50<<","<<75<<"]";
+      
+    }
+    
+    else if(n> 75.0 && n<=100.0){
+        
+            cout<<"Interval ("<<75<<","<<100<<"]";
+        
+        
+    }
+    
+    else{
+        cout<<"Out of Intervals";
+    }
+    
+    return 0;
+}
